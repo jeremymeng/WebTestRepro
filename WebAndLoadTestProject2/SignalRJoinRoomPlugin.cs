51b7002cc6459b73247d310e0a5a1e0d98a1b00e
Jeremy Meng <yumeng@microsoft.com>
2018-10-30 14:55:22 -0700

    Initial commit

---
+++ WebAndLoadTestProject2/SignalRJoinRoomPlugin.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using Microsoft.VisualStudio.TestTools.WebTesting;
+
+namespace iRel8.Service.WebTests.WebtestPlugins
+{
+    public class SignalRJoinRoomPlugin : WebTestRequestPlugin
+    {
+        public override async void PreRequest(object sender, PreRequestEventArgs e)
+        {
+            var baseUrl = "http://localhost:15935";
+
+            var connection = new HubConnectionBuilder()
+                .WithUrl($"{baseUrl}/chat")
+                .Build();
+
+            await connection.StartAsync();
+        }
+    }
+}
